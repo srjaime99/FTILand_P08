c42679319e4353caf7fe6fa764f13e89345eab48
carescme <161819281+carescme@users.noreply.github.com>
2024-05-02 10:40:47 +0200

    Ficheros creados
    Está la estructura creada.
    - El menú (header) lo he puesto que se cargue con JS, pero si no encontramos como quitar en cada página el enlace (más adelante botón, porque con lista queda mal) a la misma, habrá que ponerlo en cada página HTML.
    - CSS no he metido nada.
    - atracciones.html y espectaculos.html serán para mostrar todas las atracciones y espectaculos, y poder seleccionar.
    - Al seleccionar, la atracción o espectaculo elegido se cargará en atracción.html o espectaculo.html (NO ESTÁ IMPLEMENTADO).

---
+++ script.js
@@ -0,0 +1,30 @@
+function cargarCabecera() {
+    const header = document.createElement('div');
+    header.id = 'header';
+    header.innerHTML = `
+        <div id="header">
+            <!-- Sección Izquierda del Menú -->
+            <div id="menu-left">
+                <a href="index.html"><img src="logo.png" alt="FTILand Logo"></a>
+            </div>
+            <!-- Sección Centro del Menú -->
+            <div id="menu-center"><h1>FTILand</h1></div>
+            <!-- Sección Derecha del Menú -->
+            <div id="menu-right">
+                <ul>
+                    <li><a href="atracciones.html">Atracciones</a></li>
+                    <li><a href="espectaculos.html">Espectáculos</a></li>
+                    <li><a href="favoritos.html">Favoritos</a></li>
+                    <li><a href="sugerencias.html">Sugerencias</a></li>
+                    <!-- Cambiar más adelante por botones, probablemente. 
+                    Mirar si se puede bloquear enlace de la página actual.
+                    Si no, añadirlo a cada página quitando cada una. -->
+                </ul>
+            </div>
+        </div>
+    `;
+    document.body.insertBefore(header, document.body.firstChild);
+}
+
+// Llama a la función para cargar la cabecera cuando se cargue la página
+window.onload = cargarCabecera;
